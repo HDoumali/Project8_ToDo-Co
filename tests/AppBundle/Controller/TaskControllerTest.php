a097b6860c21520d2d3f87c237b5b8538c7e4ae8
HassanDoumali <doumalihassan4@yahoo.fr>
2018-09-18 14:15:39 +0200

    Test : Create and edit a task

--- tests/AppBundle/Controller/TaskControllerTest.php
+++ tests/AppBundle/Controller/TaskControllerTest.php
@@ -4,6 +4,7 @@ namespace tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use AppBundle\Entity\User;
+use AppBundle\Entity\Task;
 
 Class TaskControllerTest extends WebTestCase
 {
@@ -24,5 +25,63 @@ Class TaskControllerTest extends WebTestCase
 		$crawler = $client->click($link);
 
 		$this->assertSame(1, $crawler->filter('html:contains("Marquer comme faite")')->count());
+	}
+
+	public function testCreateTask()
+	{
+		$client = static::createClient();
+		$crawler = $client->request('GET', '/login');
+
+		$form = $crawler->selectButton('Se connecter')->form();
+		$form['_username'] = 'test2';
+		$form['_password'] = 'test2';
+
+		$client->submit($form);
+
+		$crawler = $client->followRedirect();
+
+		$link = $crawler->selectLink('Créer une nouvelle tâche')->link();
+		$crawler = $client->click($link);
+
+		$this->assertSame(200, $client->getResponse()->getStatusCode());
+
+		$form = $crawler->selectButton('Ajouter')->form();
+		$form['task[title]'] = 'Test create';
+		$form['task[content]'] = 'Test create';
+
+		$client->submit($form);
+
+		$crawler = $client->followRedirect();
+
+		$this->assertSame(1, $crawler->filter('div.alert.alert-success:contains("Superbe ! La tâche a été bien été ajoutée")')->count());
+	}
+
+	public function testEditTask()
+	{
+		$client = static::createClient();
+		$crawler = $client->request('GET', '/login');
+
+		$form = $crawler->selectButton('Se connecter')->form();
+		$form['_username'] = 'test2';
+		$form['_password'] = 'test2';
+
+		$client->submit($form);
+
+		$crawler = $client->followRedirect();
+        
+		$crawler = $client->request('GET', '/tasks/29/edit');
+
+		$this->assertSame(1, $crawler->filter('html:contains("Title")')->count());
+		$this->assertSame(1, $crawler->filter('html:contains("Content")')->count());
+
+		$form = $crawler->selectButton('Modifier')->form();
+		$form['task[title]'] = 'Test edit';
+		$form['task[content]'] = 'Test edit';
+
+		$client->submit($form);
+
+		$crawler = $client->followRedirect();
+
+		$this->assertSame(1, $crawler->filter('div.alert.alert-success:contains("Superbe ! La tâche a bien été modifiée.")')->count()); 
 	}
 }
